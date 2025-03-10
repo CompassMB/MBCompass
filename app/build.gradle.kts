b159641c486a76bc9e8c6b26919766a975eb42f2
ayus-aryal <ayusaryal411@gmail.com>
2025-03-11 00:01:23 +0530

    The Libraries have been updated to the latest versions and the project has been moved to the compose compiler gradle plugin.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.kotlinSymbolProcessing)
     alias(libs.plugins.jetbrains.kotlin.android)
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
-
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
 
 
