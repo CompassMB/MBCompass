2ecac206953e9402ee902818af1618e394bd7fc4
Mubarak Basha <153428452+MubarakNative@users.noreply.github.com>
2025-03-11 09:21:06 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
 
-
 }
 
 android {
@@ -53,8 +52,6 @@ android {
     buildFeatures {
         compose = true
         viewBinding = true
-    }
-    composeOptions {
     }
     packaging {
         resources {
