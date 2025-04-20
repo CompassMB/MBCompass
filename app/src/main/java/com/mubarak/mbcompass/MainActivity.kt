92063ffe24b6ecbee3bae4fde50fb73996e75a00
Mubarak <enpisb251@gmail.com>
2025-04-20 10:03:59 +0530

    Removed Maplibre dependency

--- app/src/main/java/com/mubarak/mbcompass/MainActivity.kt
+++ app/src/main/java/com/mubarak/mbcompass/MainActivity.kt
@@ -3,16 +3,15 @@
 package com.mubarak.mbcompass
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.fragment.app.FragmentActivity
 import com.mubarak.mbcompass.ui.CompassNavGraph
 import com.mubarak.mbcompass.ui.theme.MBCompassTheme
-import org.maplibre.android.MapLibre
 
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        MapLibre.getInstance(this)
         setContent {
             MBCompassTheme {
                 CompassNavGraph()
