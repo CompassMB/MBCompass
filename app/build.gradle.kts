c5a899f0e5042b99edab4bec0618ead22fc30833
Mubarak <enpisb251@gmail.com>
2025-05-11 11:18:57 +0530

    New version released v1.1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.mubarak.mbcompass"
         minSdk = 21
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.1.5"
+        versionCode = 7
+        versionName = "1.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
