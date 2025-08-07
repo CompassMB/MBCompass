1e402634d9898d08fc66fa2e73ec32ac32bc87ad
Mubarak <enpisb251@gmail.com>
2025-08-07 21:35:21 +0530

    feat: Added support page in settings

--- app/src/main/java/com/mubarak/mbcompass/utils/Const.kt
+++ app/src/main/java/com/mubarak/mbcompass/utils/Const.kt
@@ -1,7 +1,8 @@
 package com.mubarak.mbcompass.utils
 
 object Const {
-    const val APP_PAGE = "https://github.com/MubarakNative/MBCompass"
+    const val APP_PAGE = "https://github.com/CompassMB/MBCompass"
     const val LICENSE_PAGE = "https://www.gnu.org/licenses/gpl-3.0.txt"
+    const val SUPPORT_PAGE = "https://compassmb.github.io/MBCompass-site/donate.html"
     const val AUTHOR_EMAIL = "dev.mubarakbasha@gmail.com"
 }
