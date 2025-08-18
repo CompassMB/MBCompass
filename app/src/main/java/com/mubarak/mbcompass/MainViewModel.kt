c078bbebffba87f1f0b69701750670b5d4fb7fe6
Mubarak <enpisb251@gmail.com>
2025-08-18 16:02:11 +0530

    fix: Fixed unnecessary recomposition on compass rose

--- app/src/main/java/com/mubarak/mbcompass/MainViewModel.kt
+++ app/src/main/java/com/mubarak/mbcompass/MainViewModel.kt
@@ -3,22 +3,27 @@
 package com.mubarak.mbcompass
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlin.math.roundToInt
 
-class MainViewModel:ViewModel() {
+class MainViewModel : ViewModel() {
     private val _azimuth = MutableStateFlow(0F)
     val azimuth: StateFlow<Float> = _azimuth.asStateFlow()
 
     private val _strength = MutableStateFlow(0F)
     val strength: StateFlow<Float> = _strength.asStateFlow()
 
-    fun updateAzimuth(azimuth:Float){
+    fun updateAzimuth(azimuth: Float) {
         _azimuth.value = azimuth
     }
 
-    fun updateMagneticStrength(strengthInUt: Float){
+    fun updateMagneticStrength(strengthInUt: Float) {
         _strength.value = strengthInUt
     }
 
