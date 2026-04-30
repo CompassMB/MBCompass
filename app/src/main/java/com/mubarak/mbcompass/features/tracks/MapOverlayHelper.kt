f047d7cb5ae1c430a82bbbb46ba969fcdd5c8acf
Mubarak <enpisb251@gmail.com>
2026-04-30 15:29:29 +0530

    docs: update fastlane screenshots

--- app/src/main/java/com/mubarak/mbcompass/features/tracks/MapOverlayHelper.kt
+++ app/src/main/java/com/mubarak/mbcompass/features/tracks/MapOverlayHelper.kt
@@ -1,5 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+/*
+*
+* Copyright (c) 2024 Mubarak Basha. All Rights Reserved.
+This project is licensed under GPL-3.0. Any derivative work must keep the same license,
+retain this copyright notice, and provide proper attribution.
+*
+* */
 
 
 package com.mubarak.mbcompass.features.tracks
