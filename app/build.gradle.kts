5cab4510dd6822d9c583f76ba95fc63772218d98
Mubarak Basha <153428452+MubarakNative@users.noreply.github.com>
2026-05-02 16:35:31 +0530

    Fix release v1.1.15 (#158)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.mubarak.mbcompass"
         minSdk = 23
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.1.14"
+        versionCode = 16
+        versionName = "1.1.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
